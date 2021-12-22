0c50dc226592ac790396cddfb180efd8a2495959
Robert Grazioli <rjgrazioli@gmail.com>
2021-12-22 09:35:54 -0500

    added version

--- arex-survey.js
+++ arex-survey.js
@@ -1,5 +1,4 @@
 (async () => {
-  console.log("jimmmy");
   // Pulling in questions from this repo: https://github.com/sugarliving/sugar-arex-survey
   const categories = await $.getJSON(
     "https://raw.githubusercontent.com/sugarliving/sugar-arex-survey/main/categories.json"
